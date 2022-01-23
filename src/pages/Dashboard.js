2068355158a12a8db4adf3b9e75935d7239c0a2f
EliaRitzmann <E.Ritzmann.inf20@stud.bbbaden.ch>
2022-01-23 15:34:50 +0100

    first working Demo

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 const Dashboard = () => {
-  return <div></div>;
+  const {user, logOut} = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogOut(){
+    try{
+      logOut()
+      navigate("/login");
+  }catch(error){
+      console.log(error)
+  }
+  }
+
+  
+  return <div className='flex flex-col items-center mt-20'>
+    <h1 className='text-2xl font-bold'>Logged in as: {user.email}</h1>
+    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5' onClick={handleLogOut}>Log Out</button>
+    </div>;
 };
 
 export default Dashboard;
--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
-const ProtectedRoute = () => {
-  return <div></div>;
+const ProtectedRoute = ({children}) => {
+    const {user} = useAuth();
+    if(!user){
+        return <Navigate to="/login"/>
+    }
+    return children;
 };
 
 export default ProtectedRoute;
