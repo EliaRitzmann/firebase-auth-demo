fb3559abb52f82455ba25e7bc4bd9c3a04bb0eae
EliaRitzmann <E.Ritzmann.inf20@stud.bbbaden.ch>
2022-01-25 10:05:39 +0100

    fix profile picture bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { useAuth } from '../contexts/AuthContext';
+import pic from "../images/blank-profile-picture.png"
 
 export const Navbar = () => {
   const {logOut, user} = useAuth();
@@ -9,7 +10,7 @@ export const Navbar = () => {
       return(<div>
         <button type="button" onClick={handleProfile} class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" type="button" data-dropdown-toggle="dropdown">
         <span class="sr-only">Open user menu</span>
-        <img class="w-8 h-8 rounded-full" src={user ? user.photoURL : "https://powerusers.microsoft.com/t5/image/serverpage/image-id/98171iCC9A58CAF1C9B5B9/image-size/large/is-moderation-mode/true?v=v2&px=999"} alt="user photo"/>
+        <img class="w-8 h-8 rounded-full" src={user.photoURL ? user.photoURL : pic} alt="user photo"/>
       </button>
       </div>)
     }else{
